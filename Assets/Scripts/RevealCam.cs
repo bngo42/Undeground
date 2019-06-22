70ee82db5b5e789575abd5b165725d612dfa4e75
Bobi NGO <bngo@e3r6p14.42.fr>
2019-06-22 14:07:44 +0200

    Switch to coroutines

--- Assets/Scripts/RevealCam.cs
+++ Assets/Scripts/RevealCam.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class RevealCam : MonoBehaviour {
 
+	public	static	RevealCam	rc;
+	public	float	animationSpeed = 2f;
 	private	float	timer = 1f;
 	private Vector3	offset;
 
-	void Update() {
-		if (timer > 0f) {
-			timer -= (Time.deltaTime / 4f);
-			offset = transform.position + (Random.insideUnitSphere * (0.3f * timer));
-			transform.position = Vector3.Lerp (transform.position, offset, timer);
-			Camera.main.nearClipPlane = Mathf.Lerp(0f, 50f, timer);
-		} else {
-			this.enabled = false;
+	void Awake() {
+		if (rc == null) {
+			rc = this;
 		}
+	}
+
+	public IEnumerator revealLevel() {
+		GameManager.gm.canMove = false;
+		timer = 1f;
+		for(;;) {
+			timer -= (Time.deltaTime / animationSpeed);
+			updateState();
+			if (timer <= 0f) {
+				GameManager.gm.canMove = true;
+				break ;
+			}
+			yield return null;
+		}
+	}
+
+	public IEnumerator hideLevel() {
+		GameManager.gm.canMove = false;
+		timer = 0f;
+		for(;;) {
+			timer += (Time.deltaTime / animationSpeed);
+			updateState();
+			if (timer >= 1f) {
+				GameManager.gm.canMove = true;
+				break ;
+			}
+			yield return null;
+		}
+	}
+
+	private void updateState() {
+		offset = transform.position + (Random.insideUnitSphere * (0.3f * timer));
+		transform.position = Vector3.Lerp (transform.position, offset, timer);
+		Camera.main.nearClipPlane = Mathf.Lerp(0f, 50f, timer);
 	}
 }
