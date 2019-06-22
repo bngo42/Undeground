00d9c8a423e4ba78c4d2f641c5f34a1f7cf005ca
Bobi NGO <bngo@e3r6p14.42.fr>
2019-06-22 13:43:02 +0200

    Adding reveal level script

---
+++ Assets/Scripts/RevealCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealCam : MonoBehaviour {
+
+	private	float	timer = 1f;
+	private Vector3	offset;
+
+	void Update() {
+		if (timer > 0f) {
+			timer -= (Time.deltaTime / 4f);
+			offset = transform.position + (Random.insideUnitSphere * (0.3f * timer));
+			transform.position = Vector3.Lerp (transform.position, offset, timer);
+			Camera.main.nearClipPlane = Mathf.Lerp(0f, 50f, timer);
+		} else {
+			this.enabled = false;
+		}
+	}
+}
