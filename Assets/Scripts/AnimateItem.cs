fb510fe5675dbc78504e50e43b920110a3bc20d1
Bobi NGO <bngo@e3r6p14.42.fr>
2019-06-22 13:44:39 +0200

    Adding script about item

---
+++ Assets/Scripts/AnimateItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateItem : MonoBehaviour {
+
+	public float 		animationSpeed;
+	public float 		animationLength;
+	public float		xSpeedAxis;
+	public float		ySpeedAxis;
+	public float		zSpeedAxis;
+
+	private Vector3		pos;
+	private Quaternion 	rot;
+
+	void Start () {
+		pos = transform.position;
+	}
+	
+	void Update () {
+		animateCoin ();
+	}
+
+	void animateCoin(){
+		pos.y += Mathf.Sin ((Time.time) * (Time.timeScale * animationSpeed)) * (0.01f * animationLength);
+		transform.position = pos;
+		var rotation = Time.timeScale * (animationSpeed / 2);
+		transform.Rotate (new Vector3 (rotation * (xSpeedAxis * 0.1f),rotation * (ySpeedAxis * 0.1f),rotation * (zSpeedAxis * 0.1f)));
+	}
+}
---
+++ Assets/Scripts/ItemPick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPick : MonoBehaviour {
+
+	void OnTriggerStay(Collider col) {
+		if (col.tag == "Player" && col.gameObject.layer.Equals(LayerMask.NameToLayer("PlayerOverground"))) {
+			gameObject.SetActive(false);
+		}
+	}
+}
