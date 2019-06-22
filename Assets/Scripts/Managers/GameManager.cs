6b614b86a7126adb8531cc561f5f8917be701b1a
Bobi NGO <bngo@e3r6p14.42.fr>
2019-06-22 13:46:08 +0200

    Update

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+
+	void Awake () {
+		if (gm == null){
+			gm = this;
+		}	
+	}
+
+	public void pauseGame() {
+		Time.timeScale = 0f;
+	}
+
+	public void resumeGame() {
+		Time.timeScale = 1f;
+	}
+	
+}
