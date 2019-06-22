4cd66e987c82f218f6d048ced1774cbf186a1d8e
Bobi NGO <bngo@e3r6p6.42.fr>
2019-06-22 11:17:31 +0200

    Initialize project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform	target;
+	public float		damping = 0.3f;
+	private Vector3		offset;
+
+	void Start() {
+		offset = transform.localPosition;
+	}
+
+	void FixedUpdate () {
+		if (target != null) {
+			transform.position = Vector3.Lerp(transform.position, target.position + offset, damping);
+		}
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float 			playerSpeed = 1f;
+	public ParticleSystem 	particles;
+	private bool			isUnderground = false;
+	private bool			canMove = true;
+
+	private Rigidbody 		rb;
+	private Renderer		rend;
+	private Material		currentMat;
+	private IEnumerator		corrToggleMode;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rend = GetComponent<Renderer>();
+	}
+	
+	void FixedUpdate () {
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, -transform.up, out hit, 10f)) {
+			currentMat = hit.transform.GetComponent<Renderer>().material;
+
+			ParticleSystem.MainModule main = particles.main;
+			main.startColor = currentMat.color;
+		}
+		if (corrToggleMode == null && Input.GetButtonDown("Fire3")){
+			if (!hit.transform.gameObject.layer.Equals(LayerMask.NameToLayer("Floor"))){
+				isUnderground = !isUnderground;
+				Debug.Log("Switch mode !");
+				corrToggleMode = toggleMode();
+				StartCoroutine(corrToggleMode);
+			}
+
+		}
+
+		if (canMove) {
+			Vector3 vel = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+			rb.velocity = Vector3.ClampMagnitude((vel * (playerSpeed * 4f)), 4f);
+		}
+	}
+
+	public void switchLayer(string layerName) {
+		if (!string.IsNullOrEmpty(layerName)) {
+			gameObject.layer = LayerMask.NameToLayer(layerName);
+		}
+	}
+
+	IEnumerator toggleMode() {
+		canMove = false;
+		yield return new WaitForSeconds(1f);
+		rend.enabled = !isUnderground;
+		if (isUnderground){
+			switchLayer("PlayerUnderground");
+			particles.Play();
+		} else {
+			switchLayer("PlayerOverground");
+			particles.Stop();
+		}
+		canMove = true;
+		corrToggleMode = null;
+	}
+}
